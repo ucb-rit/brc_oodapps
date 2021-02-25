1441c8680ce75c1247bf4156489df19718b1a3fd
Krishna Muriki <kmuriki@lbl.gov>
2021-02-24 23:25:52 -0800

    Initial sync of all BRC OOD APPS into this single repo -krishna

---
+++ brc_rstudio-compute/README.md
@@ -0,0 +1,44 @@
+# Batch Connect - BRC RStudio Server
+
+An Batch Connect app that launches an RStudio server within a
+batch job.
+
+## Install
+
+These are command line only installation directions.
+
+We start by downloading a zipped package of this code. This allows us to start
+with a fresh directory that has no git history as we will be building off of
+this.
+
+```sh
+#Git Clone
+git clone https://github.com/ucb-rit/brc_rstudio-compute_oodapp.git
+
+# Change to this new directory
+cd brc_rstudio-compute_oodapp
+```
+
+From here you will make any modifications to the code that you would like and
+version your changes in your own repository:
+
+```sh
+#
+# Make all your code changes while testing them in the OnDemand Dashboard
+#
+# ...
+#
+
+# Add the files to the Git repository
+git add --all
+
+# Commit the staged files to the Git repository
+git commit -m "my changes"
+```
+
+## Contributing
+
+1. Create your feature branch (`git checkout -b my-new-feature`)
+2. Commit your changes (`git commit -am 'Add some feature'`)
+3. Push to the branch (`git push origin my-new-feature`)
+
---
+++ brc_rstudio-compute/form.js
@@ -0,0 +1,89 @@
+'use strict'
+
+/**
+ *   Toggle the visibility of a form group
+ *  
+ *   @param      {string}    form_id  The form identifier
+ *   @param      {boolean}   show     Whether to show or hide
+ */
+function toggle_visibility_of_form_group(form_id, show) {
+  let form_element = $(form_id);
+  let parent = form_element.parent();
+
+  if(show) {
+    parent.show();
+  } else {
+    form_element.val('');
+    parent.hide();
+  }
+}
+
+/**
+ *  Toggle the visibility of the GRES Value field
+ */
+function toggle_gres_value_field_visibility() {
+  let slurm_partition = $("#batch_connect_session_context_slurm_partition");
+  let gpu_partitions = [
+    'savio2_gpu', 'savio2_1080ti', 'savio3_gpu'
+  ];
+
+  toggle_visibility_of_form_group(
+    '#batch_connect_session_context_gres_value',
+    gpu_partitions.includes(slurm_partition.val()));
+}
+
+/**
+ * Toggle the visibility of the CPU cores field 
+ */
+function toggle_cpu_cores_field_visibility() {
+  let slurm_partition = $("#batch_connect_session_context_slurm_partition");
+  let per_core_partitions = [
+    'savio2_gpu', 'savio2_1080ti', 'savio3_gpu', 'savio2_htc', 'savio2_knl'
+  ];
+
+  toggle_visibility_of_form_group(
+    '#batch_connect_session_context_num_cores',
+    per_core_partitions.includes(slurm_partition.val()));
+}
+
+/**
+ * Toggle the visibility of the SLURM Account and QOS fields
+ */
+function toggle_slurm_account_qos_fields_visibility() {
+  let slurm_partition = $("#batch_connect_session_context_slurm_partition");
+  let non_account_partition = [ 'vector' ];
+
+  toggle_visibility_of_form_group(
+    '#batch_connect_session_context_bc_account',
+    !non_account_partition.includes(slurm_partition.val()));
+
+  toggle_visibility_of_form_group(
+    '#batch_connect_session_context_qos_name',
+    !non_account_partition.includes(slurm_partition.val()));
+}
+
+
+/**
+ * Sets the change handler for the slurm partition select.
+ */
+function set_slurm_partition_change_handler() {
+  let slurm_partition = $("#batch_connect_session_context_slurm_partition");
+  slurm_partition.change(() => {
+    toggle_gres_value_field_visibility();
+    toggle_cpu_cores_field_visibility();
+    toggle_slurm_account_qos_fields_visibility();
+  });
+}
+
+/**
+ *  Install event handlers
+ */
+$(document).ready(function() {
+  // Ensure that fields are shown or hidden based on what was set in the last session
+  toggle_gres_value_field_visibility();
+  toggle_cpu_cores_field_visibility();
+  toggle_slurm_account_qos_fields_visibility();
+
+  set_slurm_partition_change_handler();
+});
+
