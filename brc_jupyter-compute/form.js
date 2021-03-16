0a473de2dda58962763fcb62cc12ec2341c9816e
Nicolas Chan <nicolaschan@berkeley.edu>
2021-03-16 15:39:45 -0700

    Add selection filtering based on account/partition

--- brc_jupyter-compute/form.js
+++ brc_jupyter-compute/form.js
@@ -72,7 +72,55 @@ function set_slurm_partition_change_handler() {
     toggle_gres_value_field_visibility();
     toggle_cpu_cores_field_visibility();
     toggle_slurm_account_qos_fields_visibility();
+    update_available_options();
   });
+}
+
+function get_associations() {
+  const raw_data = $('#batch_connect_session_context_raw_data').val();
+  const assocs = [];
+  for (const assoc of raw_data.split(" ").filter(x => x)) {
+    const [partition, account, qos_list] = assoc.split("|");
+    assocs.push({ partition, account, qos: qos_list.split(",") });
+  }
+  return assocs;
+}
+
+function replace_options($select, new_options) {
+  const old_selection = $select.val();
+  $select.empty();
+  new_options.sort().map(option => $select.append($("<option></option>").attr("value", option).text(option)));
+  if (new_options.includes(old_selection)) {
+    $select.val(old_selection);
+  }
+}
+
+function set_available_accounts() {
+  const assocs = get_associations();
+  const accounts = [...new Set(assocs.map(({ account }) => account))];
+  replace_options($("#batch_connect_session_context_slurm_account"), accounts);
+}
+
+function set_available_partitions() {
+  let assocs = get_associations();
+  const selected_account = $("#batch_connect_session_context_slurm_account").val();
+  assocs = assocs.filter(({ account }) => account === selected_account);
+  const partitions = assocs.map(({ partition }) => partition);
+  replace_options($("#batch_connect_session_context_slurm_partition"), partitions);
+}
+
+function set_available_qos() {
+  const assocs = get_associations();
+  const selected_account = $("#batch_connect_session_context_slurm_account").val();
+  const selected_partition = $("#batch_connect_session_context_slurm_partition").val();
+  const qos = assocs
+    .filter(({ account, partition }) => (account === selected_account && partition === selected_partition))[0].qos;
+  replace_options($("#batch_connect_session_context_qos_name"), qos);
+}
+
+function update_available_options() {
+  set_available_partitions();
+  set_available_qos();
 }
 
 /**
@@ -85,5 +133,9 @@ $(document).ready(function() {
   toggle_slurm_account_qos_fields_visibility();
 
   set_slurm_partition_change_handler();
+
+  set_available_accounts();
+  $("#batch_connect_session_context_slurm_account").change(update_available_options);
+  update_available_options();
 });
 
