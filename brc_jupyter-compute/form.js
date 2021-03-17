4b9ac88cdbcf35f84d16abf727f0ac22bf9cadb2
Krishna Muriki <kmuriki@lbl.gov>
2021-03-16 23:18:48 -0700

    Seeting the order of loading options from Partition to Account to Qos -krishna

--- brc_jupyter-compute/form.js
+++ brc_jupyter-compute/form.js
@@ -18,64 +18,6 @@ function toggle_visibility_of_form_group(form_id, show) {
   }
 }
 
-/**
- *  Toggle the visibility of the GRES Value field
- */
-function toggle_gres_value_field_visibility() {
-  let slurm_partition = $("#batch_connect_session_context_slurm_partition");
-  let gpu_partitions = [
-    'savio2_gpu', 'savio2_1080ti', 'savio3_gpu'
-  ];
-
-  toggle_visibility_of_form_group(
-    '#batch_connect_session_context_gres_value',
-    gpu_partitions.includes(slurm_partition.val()));
-}
-
-/**
- * Toggle the visibility of the CPU cores field 
- */
-function toggle_cpu_cores_field_visibility() {
-  let slurm_partition = $("#batch_connect_session_context_slurm_partition");
-  let per_core_partitions = [
-    'savio2_gpu', 'savio2_1080ti', 'savio3_gpu', 'savio2_htc', 'savio2_knl', 'savio3_htc'
-  ];
-
-  toggle_visibility_of_form_group(
-    '#batch_connect_session_context_num_cores',
-    per_core_partitions.includes(slurm_partition.val()));
-}
-
-/**
- * Toggle the visibility of the SLURM Account and QOS fields
- */
-function toggle_slurm_account_qos_fields_visibility() {
-  let slurm_partition = $("#batch_connect_session_context_slurm_partition");
-  let non_account_partition = [ 'vector' ];
-
-  toggle_visibility_of_form_group(
-    '#batch_connect_session_context_bc_account',
-    !non_account_partition.includes(slurm_partition.val()));
-
-  toggle_visibility_of_form_group(
-    '#batch_connect_session_context_qos_name',
-    !non_account_partition.includes(slurm_partition.val()));
-}
-
-
-/**
- * Sets the change handler for the slurm partition select.
- */
-function set_slurm_partition_change_handler() {
-  let slurm_partition = $("#batch_connect_session_context_slurm_partition");
-  slurm_partition.change(() => {
-    toggle_gres_value_field_visibility();
-    toggle_cpu_cores_field_visibility();
-    toggle_slurm_account_qos_fields_visibility();
-    update_available_options();
-  });
-}
-
 function get_associations() {
   const raw_data = $('#batch_connect_session_context_raw_data').val();
   const assocs = [];
@@ -95,47 +37,108 @@ function replace_options($select, new_options) {
   }
 }
 
-function set_available_accounts() {
-  const assocs = get_associations();
-  const accounts = [...new Set(assocs.map(({ account }) => account))];
-  replace_options($("#batch_connect_session_context_slurm_account"), accounts);
+/**
+ *  Toggle the visibility of the GRES Value field
+ */
+function toggle_gres_value_field_visibility() {
+  let slurm_partition = $("#batch_connect_session_context_slurm_partition");
+  let gpu_partitions = [
+    'savio2_gpu', 'savio2_1080ti', 'savio3_gpu', 'savio3_2080ti'
+  ];
+
+  toggle_visibility_of_form_group(
+    '#batch_connect_session_context_gres_value',
+    gpu_partitions.includes(slurm_partition.val()));
 }
 
-function set_available_partitions() {
+/**
+ * Toggle the visibility of the CPU cores field 
+ */
+function toggle_cpu_cores_field_visibility() {
+  let slurm_partition = $("#batch_connect_session_context_slurm_partition");
+  let per_core_partitions = [
+    'savio2_gpu', 'savio2_1080ti', 'savio3_gpu', 'savio2_htc', 'savio2_knl', 'savio3_htc', 'savio3_2080ti'
+  ];
+
+  toggle_visibility_of_form_group(
+    '#batch_connect_session_context_num_cores',
+    per_core_partitions.includes(slurm_partition.val()));
+}
+
+/**
+ * Toggle the visibility of the SLURM Account and QOS fields
+ */
+function toggle_slurm_account_qos_fields_visibility() {
+  let slurm_partition = $("#batch_connect_session_context_slurm_partition");
+  let non_account_partition = [ 'vector' ];
+
+  toggle_visibility_of_form_group(
+    '#batch_connect_session_context_slurm_account',
+    !non_account_partition.includes(slurm_partition.val()));
+
+  toggle_visibility_of_form_group(
+    '#batch_connect_session_context_qos_name',
+    !non_account_partition.includes(slurm_partition.val()));
+}
+
+function set_available_accounts() {
   let assocs = get_associations();
-  const selected_account = $("#batch_connect_session_context_slurm_account").val();
-  assocs = assocs.filter(({ account }) => account === selected_account);
-  const partitions = assocs.map(({ partition }) => partition);
-  replace_options($("#batch_connect_session_context_slurm_partition"), partitions);
+  const selected_partition = $("#batch_connect_session_context_slurm_partition").val();
+  assocs = assocs.filter(({ partition }) => partition === selected_partition);
+  const accounts = assocs.map(({ account }) => account);
+  replace_options($("#batch_connect_session_context_slurm_account"), accounts);
 }
 
 function set_available_qos() {
   const assocs = get_associations();
   const selected_account = $("#batch_connect_session_context_slurm_account").val();
   const selected_partition = $("#batch_connect_session_context_slurm_partition").val();
-  const qos = assocs
+  const allqos = assocs
     .filter(({ account, partition }) => (account === selected_account && partition === selected_partition))[0].qos;
+  const non_existent_qos = 'normal';
+  const qos = allqos.filter(qos => qos !== non_existent_qos);
   replace_options($("#batch_connect_session_context_qos_name"), qos);
 }
 
 function update_available_options() {
-  set_available_partitions();
+  set_available_accounts();
   set_available_qos();
+}
+
+
+/**
+ * Sets the change handler for the slurm partition select.
+ */
+function set_slurm_partition_change_handler() {
+  let slurm_partition = $("#batch_connect_session_context_slurm_partition");
+  slurm_partition.change(() => {
+    toggle_gres_value_field_visibility();
+    toggle_cpu_cores_field_visibility();
+    toggle_slurm_account_qos_fields_visibility();
+    update_available_options();
+  });
+}
+
+function set_available_partitions() {
+  const assocs = get_associations();
+  const allpartitions = [...new Set(assocs.map(({ partition }) => partition))];
+  // Skip on partition value of cortex as that is an obsolete partition now
+  const non_existent_partition = 'cortex';
+  const partitions = allpartitions.filter(partition => partition !== non_existent_partition);
+  replace_options($("#batch_connect_session_context_slurm_partition"), partitions);
 }
 
 /**
  *  Install event handlers
  */
 $(document).ready(function() {
+  set_available_partitions();
   // Ensure that fields are shown or hidden based on what was set in the last session
   toggle_gres_value_field_visibility();
   toggle_cpu_cores_field_visibility();
   toggle_slurm_account_qos_fields_visibility();
+  // Update available options appropriately
+  update_available_options();
 
   set_slurm_partition_change_handler();
-
-  set_available_accounts();
-  $("#batch_connect_session_context_slurm_account").change(update_available_options);
-  update_available_options();
 });
-
